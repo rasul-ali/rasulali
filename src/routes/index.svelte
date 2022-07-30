5ebc9d9ba0dbef8fbdfb69d18ee14eb197e0bf3d
rasul-ali <resul.eliyev2000@mail.ru>
2022-07-30 15:52:49 +0400

    starting

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+    <title>Rasul Ali</title>
+    <meta name="description" content="a personal blog" />
+</svelte:head>
+<div>
+    <div class="flex flex-row" >
+        <h1 class="font-bold text-4xl m-1">O</h1>
+        <nav class="mx-auto flex flex-row gap-4 m-1">
+            <h1 class="text-3xl">test</h1>
+            <h1 class="text-3xl">test</h1>
+            <h1 class="text-3xl">test</h1>
+            <h1 class="text-3xl">test</h1>
+        </nav>
+        <h1 class="font-mono text-2xl m-1">Log In</h1>
+    </div>
+    <div class="mx-10 mt-10 shadow-xl w-9/10 min-h-screen"></div>
+</div>
